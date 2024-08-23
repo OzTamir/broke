d11e88cbac8af05ae1228c23baba7b8ff4282662
Oz Tamir <oz@tamir.fun>
2024-08-23 16:48:03 +0300

    Initial Commit

---
+++ Broke/BrokeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrokeApp.swift
+//  Broke
+//
+//  Created by Oz Tamir on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrokeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
